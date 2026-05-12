78e9988f99a48c7f42eaad2ed08ce37046f84067
Atakan <atakanerdonmez06@gmail.com>
2026-05-12 14:23:24 +0000

    fix: Change the name to Kbas for responsiveness

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "KnowledgeBase",
+    pageTitle: "KBase",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
