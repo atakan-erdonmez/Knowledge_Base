da934040005a38bdf0503dafd1523f77b75879b5
Atakan <atakanerdonmez06@gmail.com>
2026-05-12 09:17:13 +0000

    fix: typo

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -39,7 +39,7 @@ export const defaultContentPageLayout: PageLayout = {
       ],
     }),
     Component.Explorer(),
-    Component.RecentNotes,
+    Component.RecentNotes(),
   ],
   right: [
     Component.Graph(),
